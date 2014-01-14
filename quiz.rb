c47b338649b40f2b7ed04efe008c6021df03c9a3
Alex Grant <grantovich@users.noreply.github.com>
2014-01-14 08:54:25 -0500

    Use consistent terminology

--- quiz.rb
+++ quiz.rb
@@ -5,31 +5,31 @@
 #     |  |     |  |  |  | |  | .----)   |      |  | .----)   |       /  _____  \     |  `--'  '--.|  `--'  | |  |   /  /----.__
 #     |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
 
-# Question 1: a function called sleep_in?
+# Question 1: a method called sleep_in?
 #############
-# Write a function called sleep_in?
+# Write a method called sleep_in?
 # You can sleep in if it is not a weekday or if you are on vacation.
 # sleep_in? => false
 # sleep_in?(vacation: true) => true
 def sleep_in?
 end
 
-# Question 2: a function called del_del
+# Question 2: a method called del_del
 #############
 # Remove "del" from a string.
 # del_del("abdelcd") => "abcd"
 # del_del("xyz") => "xyz"
 
-# Question 3: a function called missingChar
+# Question 3: a method called missing_char
 #############
 # Remove the character that corresponds to the index from the string.
 # If you don't enter a string
 # missing_char("kitten", 1) => "ktten"
 # missing_char(347, 1) => RuntimeError: Please enter a string!
 
-# Question 4: a function called nearHundred
+# Question 4: a method called near_hundred
 #############
-# Write a function called nearHundred
+# Write a method called near_hundred
 # If the number is between 90 and 99, the result is true;
 # If it is 89 or below, it is false.
 # 93.near_hundred? => true
